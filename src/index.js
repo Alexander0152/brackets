eaef1dfa32cbb59338eba65a3cb174408d91b57e
Alexander0152 <66972872+Alexander0152@users.noreply.github.com>
2021-02-02 18:34:27 +0300

    fix: fix solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var substrings = [];
+  for (var j = 0; j < bracketsConfig.length; j++) {
+
+    for (var i = 0; i < bracketsConfig.length; i++) {
+      substrings[i] = bracketsConfig[i][0] + bracketsConfig[i][1];
+    }
+
+    for (let k = 0; k < substrings.length; k++) {
+      if (str.includes(substrings[k])) {
+        str = str.replace(substrings[k], "");
+        k = -1;
+      }
+    }
+  }
+  if (str === "") {
+    return true;
+  }
+  return false
 }
